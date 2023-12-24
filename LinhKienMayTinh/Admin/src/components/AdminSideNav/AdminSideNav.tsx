7ec31982c5e8acd0983530a179f1f500b40cb6d8
thuyty <thuyty1411@gmail.com>
2023-12-24 14:32:02 +0700

    24/12/2023

--- LinhKienMayTinh/Admin/src/components/AdminSideNav/AdminSideNav.tsx
+++ LinhKienMayTinh/Admin/src/components/AdminSideNav/AdminSideNav.tsx
@@ -3,7 +3,24 @@ import classNames from 'classnames'
 import path from 'src/constants/path'
 export default function AdminSideNav() {
   return (
-    <div>
+    <div className='pt-5'>
+      <button className='bg-orange p-2 rounded-md hover:bg-orange-dark text-white focus:outline-none focus:shadow-outline-orange flex items-center'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-6 h-6 mr-2'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9'
+          />
+        </svg>
+        Logout
+      </button>
       <div className='mt-7'>
         <NavLink
           to={path.dashboard}
@@ -136,7 +153,7 @@ export default function AdminSideNav() {
           </div>
           Quản lý người dùng
         </NavLink>
-         <NavLink
+        <NavLink
           to={path.productreviews}
           className={({ isActive }) =>
             classNames('flex items-center capitalize transition-colors mt-6', {
